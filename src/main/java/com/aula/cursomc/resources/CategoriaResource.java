932543bb6129c37d6cd05ea53701cf1b865c9218
Matheus Morais <morais629@gmail.com>
2020-05-12 14:44:37 -0300

    Criando primeira classe e o resource

---
+++ src/main/java/com/aula/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,32 @@
+package com.aula.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aula.cursomc.models.Categoria;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Categoria> listar() {
+        
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
+
+
+}
