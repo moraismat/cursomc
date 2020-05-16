869c8ec44eb2a8744bb6e9fab6ffd6e06a8a721f
Matheus Morais <morais629@gmail.com>
2020-05-16 15:22:43 -0300

    SubTotal e Total do pedido

--- src/main/java/com/aula/cursomc/domain/Pedido.java
+++ src/main/java/com/aula/cursomc/domain/Pedido.java
@@ -54,6 +54,13 @@ public class Pedido implements Serializable{
 		this.enderecoEntrega = enderecoEntrega;
 	}
 
+	public double getTotal(){
+		double soma = 0.0;
+		for(ItemPedido ip : itens) soma+=ip.getSubTotal();
+
+		return soma;
+	}
+
 	public Integer getId() {
 		return id;
 	}
